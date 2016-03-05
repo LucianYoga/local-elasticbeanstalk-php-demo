7abbeabbc8be9887307286abfb262ec39a3e3a86
Jason Darwin <jcdarwin@gmail.com>
2016-03-06 12:08:56 +1300

    Use supervisor

--- production/boot.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Welcome to the mebooks/apache-php5 docker container"
-echo "Add your commands in boot.sh"
---
+++ production/init.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+
+# Check http://geoffrey.io/a-php-development-environment-with-docker.html
+# and https://github.com/eugeneware/docker-apache-php/blob/master/start.sh
+# for more ideas as to how to use an init script.
+echo "Welcome to the mebooks/apache-php5 docker container"
+echo "Add your commands in init.sh"
